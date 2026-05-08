ab5356b18c6439fb9010e4f471008758cb30552f
p4ntimus <p4ntimus@gmail.com>
2026-05-08 18:57:35 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ document.write('<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-
 
 // Firebase Config
 var firebaseConfig = {
-    apiKey: "AIzaSyC-hBSCx7SICKQ7ntYFcJIwgR5zsewj8hg",
-    authDomain: "pantixs.firebaseapp.com",
-    projectId: "pantixs",
-    storageBucket: "pantixs.firebasestorage.app",
-    messagingSenderId: "397472052167",
-    appId: "1:397472052167:web:b769b80410f86711cd9fe2",
-    measurementId: "G-2VR1G8JN1B"
+  apiKey: "AIzaSyC-hBSCx7SICKQ7ntYFcJIwgR5zsewj8hg",
+  authDomain: "pantixs.firebaseapp.com",
+  projectId: "pantixs",
+  storageBucket: "pantixs.firebasestorage.app",
+  messagingSenderId: "397472052167",
+  appId: "1:397472052167:web:b769b80410f86711cd9fe2",
+  measurementId: "G-2VR1G8JN1B"
 };
 
 // Initialisieren
