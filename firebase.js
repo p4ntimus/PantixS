056e838a333d3543db66a26e68c085d485aa5aee
p4ntimus <p4ntimus@gmail.com>
2026-05-08 18:17:32 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,10 @@
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script>
+// Firebase CDN laden
+document.write('<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"><\/script>');
+document.write('<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"><\/script>');
+document.write('<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"><\/script>');
 
-<script>
-  var firebaseConfig = {
+// Firebase Config
+var firebaseConfig = {
     apiKey: "AIzaSyC-hBSCx7SICKQ7ntYFcJIwgR5zsewj8hg",
     authDomain: "pantixs.firebaseapp.com",
     projectId: "pantixs",
@@ -11,10 +12,11 @@
     messagingSenderId: "397472052167",
     appId: "1:397472052167:web:b769b80410f86711cd9fe2",
     measurementId: "G-2VR1G8JN1B"
-  };
+};
 
-  firebase.initializeApp(firebaseConfig);
-
-  const auth = firebase.auth();
-  const db = firebase.firestore();
-</script>
+// Initialisieren
+setTimeout(() => {
+    firebase.initializeApp(firebaseConfig);
+    window.auth = firebase.auth();
+    window.db = firebase.firestore();
+}, 200);
